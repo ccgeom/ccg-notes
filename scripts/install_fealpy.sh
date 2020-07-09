0e9e656230fe4c46580e164efbd501a9c24e69b9
mingli <mingli.yuan@gmail.com>
2020-07-09 16:08:05 +0800

    add debug code

--- scripts/install_fealpy.sh
+++ scripts/install_fealpy.sh
@@ -3,6 +3,7 @@
 if [ ! -f .py/lib/python${PYV}/site-packages/fealpy ]; then
     echo 'FEALPy was not installed. Installing...'
     git submodule init
+    git submodule update
     cd vendors/fealpy
     pwd
     python3 setup_linux.py develop
