088d22ca59f6e107cf0fe5a72202c15ad34ff20f
mingli <mingli.yuan@gmail.com>
2020-07-09 15:57:13 +0800

    install fealpy

--- src/tools/converter.py
+++ src/tools/converter.py
@@ -3,16 +3,17 @@ import numpy as np
 import scipy.io
 
 from fealpy.mesh import TriangleMesh
-from fealpy.mesh import HalfEdgeMesh2d
-from fealpy.geometry import SphereSurface
 from fealpy.writer import MeshWriter
 
 
-
-def mat2vtk(mfile):
-    data = scipy.io.loadmat('data/doubletorus.mat')
+def mat2vtk(mfile, vfile):
+    data = scipy.io.loadmat(mfile)
     node = np.array(data['node'], dtype=np.float64)
     cell = np.array(data['elem'] - 1, dtype=np.int_)
     mesh = TriangleMesh(node, cell)
+    writer = MeshWriter(mesh)
+    writer.write(fname=vfile)
+
+
 
 
--- src/tools/viewer.py
+++ src/tools/viewer.py
@@ -1,16 +1,5 @@
-import scipy.io
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
+import tools.converter as cvt
+import pyvista as pv
 
-data = scipy.io.loadmat('data/doubletorus.mat')
-X = data['X']
-Y = data['Y']
-U = data['U']
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X, Y, U, rstride=1, cstride=1, cmap=cm.jet)
-fig.colorbar(surf)
-
-plt.show()
+mesh = pv.read('data/doubletorus.vtu')
+mesh.plot(screenshot='data/doubletorus.png')
