8bdabcd9d0ecdf2bfbc47ef21d147ad5e87f7df2
Mingli Yuan <mingli.yuan@gmail.com>
2022-07-05 09:21:03 +0800

    rollback pyvist

--- src/tools/viewer.py
+++ src/tools/viewer.py
@@ -1,13 +1,4 @@
-from vedo import *
+import pyvista as pv
 
-mesh = Mesh('data/eight.vtk')
-mesh.c('white').lighting('glossy')
-
-p1 = Point([0, +1, 0], c='yellow')
-p2 = Point([0, -1, 0], c='yellow')
-
-l1 = Light(p1, c='yellow')
-l2 = Light(p2, c='yellow')
-
-show(mesh, l1, l2, p1, p2, "eight", axes=True)
-io.screenshot('eight.png')
+mesh = pv.read('data/eight.vtk')
+mesh.plot(screenshot='data/eight.png')
